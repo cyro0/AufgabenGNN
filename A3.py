04a03e241d5f1f0ea470902fe00d5d123d18d184
cyro0 <chefbigpasta@gmx.net>
2024-05-13 19:46:15 +0200

    A3 completed

--- A3.py
+++ A3.py
@@ -1,86 +1,93 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from tensorflow.keras.datasets import mnist
 
+from keras.datasets import mnist
 (x_train, _), (x_test, _) = mnist.load_data()
+x_train = (x_train / 255).reshape(-1, 784)
+x_test = (x_test / 255).reshape(-1, 784)
 
-# Normalisierung der Daten auf Werte zwischen 0 und 1
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
+def sigmoid(z, d=False):
+    return sigmoid(z) * (1 - sigmoid(z)) + 1e-12 if d else 1 / (1 + np.exp(-z))
 
-# Anzahl der sichtbaren Neuronen (entspricht der Anzahl der Pixel in einem MNIST-Bild)
-num_visible = 28
+def relu(z, d=False):
+    return (z > 0)+1e-12 if d else  z * (z > 0)
 
-# Anzahl der verborgenen Neuronen
-num_hidden = 28
+layers = [
+    # activation, shape:(out,in)
+    {"act":relu, "shape":(1024,784)},
+    {"act":relu, "shape":(50,1024)},
+    {"act":relu, "shape":(1024,50)},
+    {"act":sigmoid, "shape":(784,1024)}
+]
 
-# Gewichte und Biases initialisieren
-W = np.random.randn(num_visible, num_hidden) * 0.01
-vb = np.zeros(num_visible)
-hb = np.zeros(num_hidden)
+# global properties
+l, errors, epochs = len(layers), [], 30
+# adam properties
+lr, b1, b2 = 0.002, 0.9, 0.999
+rw,mw,rb,mb = {},{},{},{}
+# layer properties
+a,w,b,f, = {},{},{},{}
+for i, layer in zip(range(1,l+1), layers):
+    n_out, n_in = layer["shape"]
+    f[i] = layer["act"]
+    # Xavier Initialization of weights
+    w[i] = np.random.randn(n_out, n_in) / n_in**0.5
+    b[i], rb[i], mb[i] = [np.zeros((n_out,1)) for i in [1,2,3]]
+    rw[i], mw[i] = [np.zeros((n_out, n_in)) for i in [1,2]]
 
-# Lernrate
-learning_rate = 0.01
+for t in range(1, epochs+1):
+    # Train
+    for batch in np.split(x_train, 30):
+        # Forward pass
+        a[0] = batch.T
+        for i in range(1,l+1):
+            a[i] = f[i]((w[i] @ a[i-1]) + b[i])
+        # Backpropagation
+        dz,dw,db = {},{},{}
+        for i in range(1,l+1)[::-1]:
+            d = w[i+1].T @ dz[i+1] if l-i else 0.5*(a[l]-a[0])
+            dz[i] = d * f[i](a[i],d=1)
+            dw[i] = dz[i] @ a[i-1].T
+            db[i] = np.sum(dz[i], 1, keepdims=True)
+        # Adam updates
+        def adam(m, r, z, dz, i):
+            m[i] = b1 * m[i] + (1 - b1) * dz[i]
+            r[i] = b2 * r[i] + (1 - b2) * dz[i]**2
+            m_hat = m[i] / (1. - b1**t)
+            r_hat = r[i] / (1. - b2**t) 
+            z[i] -= lr * m_hat / (r_hat**0.5 + 1e-12)
+        for i in range(1,l+1):
+            adam(mw, rw, w, dw, i)
+            adam(mb, rb, b, db, i)
+    # Validate
+    a[0] = x_test.T
+    for i in range(1,l+1):
+        a[i] = f[i]((w[i] @ a[i-1]) + b[i])
+    errors += [np.mean((a[l]-a[0])**2)]
+    print("Val loss - ", errors[-1])
 
-# Anzahl der Epochen
-num_epochs = 10
+import matplotlib.pyplot as plt
+y_pred = []
+a[0] = x_train[:20].T
+#forward pass
+for i in range(1,l+1):
+    a[i] = f[i](w[i] @ a[i-1] + b[i])
+y_pred = a[l]
 
-# Trainingsschleife
-for epoch in range(num_epochs):
-  for i in range(len(x_train)):
-    # Aktivierung der sichtbaren Schicht
-    v = x_train[i]
+plt.figure(figsize=(20,5))
 
-    # Transponieren von v, um die Dimensionen an W anzupassen
-    v = v.T
+for i in range(20):
+    plt.subplot(3, 20, i + 1)
+    plt.imshow(x_train[i].reshape(28,28), cmap="gray")
+    plt.axis("off")
 
-    # Aktivierung der verborgenen Schicht
-    h = 1 / (1 + np.exp(-np.dot(v, W) - hb))
+for i in range(20):
+    plt.subplot(3, 20, i + 1 + 20)
+    plt.imshow(a[l-2].T[i].reshape(5,-1), cmap="gray")
+    plt.axis("off")
+    
+for i in range(20):
+    plt.subplot(3, 20, i + 1 + 40)
+    plt.imshow(y_pred.T[i].reshape(28,28), cmap="gray")
+    plt.axis("off")
 
-    # Rekonstruktion der sichtbaren Schicht
-    v_prime = 1 / (1 + np.exp(-np.dot(h, W.T) - vb))
-
-    # Fehler berechnen
-    error = np.mean((v - v_prime)**2)
-
-    # Update der Gewichte und Biases
-    dW = np.dot(v.T, h) - np.dot(v_prime.T, h)
-    dvb = np.mean(v - v_prime)
-    dhb = np.mean(h - v_prime)
-
-    W += learning_rate * dW
-    vb += learning_rate * dvb
-    hb += learning_rate * dhb
-
-# Testen der RBM
-rekonstruierte_bilder = []
-for i in range(10):
-  # Aktivierung der sichtbaren Schicht
-  v = x_test[i]
-
-  # Transponieren von v, um die Dimensionen an W anzupassen
-  v = v.T
-
-  # Aktivierung der verborgenen Schicht
-  h = 1 / (1 + np.exp(-np.dot(v, W) - hb))
-
-  # Rekonstruktion der sichtbaren Schicht
-  v_prime = 1 / (1 + np.exp(-np.dot(h, W.T) - vb))
-
-  # Rekonstruiertes Bild visualisieren
-  rekonstruierte_bilder.append(v_prime.T)  # Transponieren von v_prime zurück
-
-fig, axes = plt.subplots(2, 5, figsize=(10, 5))
-for i in range(10):
-  axes[i // 5, i % 5].imshow(x_test[i], cmap='gray')
-  axes[i // 5, i % 5].axis('off')
-
-fig.suptitle('Originale MNIST-Bilder')
-
-fig, axes = plt.subplots(2, 5, figsize=(10, 5))
-for i in range(10):
-  axes[i // 5, i % 5].imshow(rekonstruierte_bilder[i], cmap='gray')
-  axes[i // 5, i % 5].axis('off')
-
-fig.suptitle('Rekonstruierte Bilder mit RBM')
 plt.show()
--- test.py
+++ test.py
@@ -1,62 +1,91 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+# Laden der MNIST-Daten
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# Normalisieren der Daten
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28*28))
+x_test = np.reshape(x_test, (len(x_test), 28*28))
+
+# Definition der RBM-Klasse
+class RBM(object):
+    def __init__(self, input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+        self.W = tf.Variable(tf.random.normal([input_size, output_size], stddev=0.01))
+        self.h_bias = tf.Variable(tf.zeros([output_size]))
+        self.v_bias = tf.Variable(tf.zeros([input_size]))
+
+    def prob_h_given_v(self, visible, W, h_bias):
+        return tf.nn.sigmoid(tf.matmul(visible, W) + h_bias)
+
+    def prob_v_given_h(self, hidden, W, v_bias):
+        return tf.nn.sigmoid(tf.matmul(hidden, tf.transpose(W)) + v_bias)
+
+    def sample_prob(self, probs):
+        return tf.nn.relu(tf.sign(probs - tf.random.uniform(tf.shape(probs))))
+
+    def gibbs_sampling(self, visible):
+        h_sample = self.sample_prob(self.prob_h_given_v(visible, self.W, self.h_bias))
+        v_sample = self.sample_prob(self.prob_v_given_h(h_sample, self.W, self.v_bias))
+        return v_sample
+
+    def train(self, X, lr=0.1, k=1):
+        positive_grad = tf.matmul(tf.transpose(X), self.prob_h_given_v(X, self.W, self.h_bias))
+        for i in range(k):
+            hidden = self.sample_prob(self.prob_h_given_v(X, self.W, self.h_bias))
+            visible = self.sample_prob(self.prob_v_given_h(hidden, self.W, self.v_bias))
+            negative_grad = tf.matmul(tf.transpose(visible), self.prob_h_given_v(visible, self.W, self.h_bias))
+            self.W += lr * (positive_grad - negative_grad) / tf.cast(tf.shape(X)[0], dtype=tf.float32)
+            self.v_bias += lr * tf.reduce_mean(X - visible, 0)
+            self.h_bias += lr * tf.reduce_mean(self.prob_h_given_v(X, self.W, self.h_bias) - self.prob_h_given_v(visible, self.W, self.h_bias), 0)
+
+# Hyperparameter
+learning_rate = 0.1
+training_epochs = 50
+batch_size = 64
+n_visible = 28*28
+n_hidden = 128
+
+# Erstellen und trainieren der RBM
+rbm = RBM(n_visible, n_hidden)
+for epoch in range(training_epochs):
+    for i in range(0, len(x_train), batch_size):
+        batch_xs = x_train[i:i+batch_size]
+        rbm.train(batch_xs, lr=learning_rate)
+
+    # Ausgabe während des Trainings
+    print("Epoch:", '%d' % (epoch+1))
+
+# Rekonstruktion der Daten
+reconstructed_imgs = []
+for i in range(len(x_test)):
+    reconstructed_img = rbm.gibbs_sampling(tf.reshape(x_test[i], (1, n_visible)))
+    reconstructed_imgs.append(reconstructed_img)
+
+reconstructed_imgs = np.reshape(reconstructed_imgs, (len(x_test), 28, 28))
+
+# Beispielrekonstruktion anzeigen
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Originalbild
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
-#                 BIAS,x,y
-train = np.array([[1, 0, 0],
-                  [1, 1, 0],
-                  [1, 0, 1],
-                  [1, 1, 1]])
-target = np.array([0, 0, 0, 1])  # AND Operation
-out = np.array([0, 0, 0, 0])
-weight = np.random.rand(3) * (0.5)
-learnrate = 1
-grad = np.zeros(3)
-
-def step(x): # Stepfunktion für output
-    return np.where(x>0, 1, 0)
-
-def learn():
-    global train, weight, out, target, learnrate
-
-    out = step(np.matmul(train, weight))
-    for i in range(len(train)):
-        if train[i].any() == 1: # Nur bei einer 1 am Eingang hilft Änderung der Gewichte
-            if out[i] < target[i]: # Vergrößere Gewicht wenn Ausgabe kleiner als Target
-                weight += learnrate * train[i]
-            elif out[i] > target[i]: # Verkleinere Gewicht wenn Ausgabe größer als Target
-                weight -= learnrate * train[i]
-
-
-def outp(N=100):  # Daten für die Ausgabefunktion generieren
-    global weight
-    x = np.linspace(0, 1, N)
-    y = np.linspace(0, 1, N)
-    xx, yy = np.meshgrid(x, y)
-    oo = step(weight[0] + weight[1] * xx + weight[2] * yy)
-    return xx, yy, oo
-
-
-def on_close(event):  # Fenster schließen
-    exit(0)
-
-
-plt.ion()
-fig = plt.figure()
-fig.canvas.mpl_connect('close_event', on_close)
-while True:  # Endlosschleife
-    learn()  # lerne einen Schritt
-    plt.clf()  # Bildschirm löschen
-    X, Y, Z = outp()  # generiere Plotdaten
-    ax = fig.add_subplot(111, projection='3d')
-    # 3D plot von den Daten
-    ax.plot_surface(X, Y, Z, edgecolor='royalblue',
-                    lw=0.5, rstride=8, cstride=8, alpha=0.3)
-    ax.set_title('Neuron lernt AND-Funktion')
-    ax.set_xlabel('In[1]')
-    ax.set_ylabel('In[2]')
-    ax.set_zlabel('Ausgabe\ndes Neurons')
-    ax.set_zlim(0, 1)
-    plt.draw()
-    plt.pause(0.00001)
+    # Rekonstruiertes Bild
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(reconstructed_imgs[i])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
